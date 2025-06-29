7f9cc390d24f23b09bf4461df4a2543bedc1198e
Mayank Kumar <mayankkumar82488@gmail.com>
2025-06-29 21:09:56 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './models/task.models';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  tasks: Task[] = [];
+  taskName: String = '';
+  dueDate: string = '';
+  addTask(): void {
+    console.log("Hello world");
+    if (this.taskName.trim() && this.dueDate.trim()) {
+      const newTask: Task = {
+        id: Date.now(),
+        name: this.taskName.trim(),
+        completed: false,
+        date: this.dueDate,
+      };
+
+      this.tasks.push(newTask);
+      this.saveTasks();
+
+      this.taskName = '';
+      this.dueDate = '';
+    }
+  }
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter(task => task.id != id)
+    this.saveTasks();
+  }
+
+  saveTasks(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+  ngOnInit(): void {
+    const local = localStorage.getItem('tasks');
+
+    if (local) {
+      this.tasks = JSON.parse(local);
+    }
+  }
 }
